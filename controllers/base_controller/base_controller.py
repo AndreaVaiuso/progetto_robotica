e76ea35908da1d200cfc706e65980f0e3b019996
salvatore-drago <salvatore.drago06@community.unipa.it>
2022-01-07 15:11:22 +0100

    Revisione Comunicazione

--- controllers/base_controller/base_controller.py
+++ controllers/base_controller/base_controller.py
@@ -11,16 +11,18 @@ battery = 100
 BASE_COORDS = {"0":[1,2,3]}
 
 
-SUPERVISOR_EMITTER_CHANNEL = 0
-GENERAL_CHANNEL = 1
+# SUPERVISOR_EMITTER_CHANNEL = 0 non capisco il supervisor deve emettere in broadcast, ogni robot ha un suo canale di ricezione
+
 
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 receiver = robot.getDevice("receiver")
 emitter = robot.getDevice("emitter")
-receiver.setChannel(SUPERVISOR_EMITTER_CHANNEL)
 receiver.enable(timestep)
+nome = robot.get_name()
+canale_di_ricezione =int(nome[(len(nome)-1])
+receiver.setChannel(canale_di_ricezione)
 state = "check_new_orders"
 
 def score_calculator(datalist):
@@ -29,15 +31,17 @@ def score_calculator(datalist):
 def update_orders():
     global battery, orders
     while True:
-        if receiver.getQueueLength() > 0:
+        if receiver.getQueueLength() > 0: #ma dobbiamo distinguere fra due tipi di dati in arrivo , i nuovi ordini, e i punteggi
             x = receiver.getData()
-            # [ "chd" , ORDER_ID , WEIGHT , DESTINATION , BASE ]
-            dataList = struct.unpack("chd",x)
+            # [ "chd" , TYPE, ORDER_ID , WEIGHT , DESTINATION , BASE ]
+            dataList = struct.unpack("chd",x) #chd è il formato di compressione
             receiver.nextPacket()
         score = score_calculator(dataList) # fare la funzione 
-        message = struct.pack("chd", DRONE_ID, score)
-        emitter.setChannel(GENERAL_CHANNEL)
-        emitter.send(message)
+        #ci serve anche ORDER_ID per capire quello score a cosa si riferisce
+        message = struct.pack("chd", DRONE_ID, dataList[1],  score) #dataList è una tupla
+        emitter.setChannel(CHANNEL_BROADCAST) #dovrebbe inviare su tutti i canali cosi
+        while emitter.send(message) != 1 : #se restituisce 0 significa che la coda di invio era piena e lo deve rifare
+          print()
         
 
 
