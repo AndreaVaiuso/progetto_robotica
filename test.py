6f575b19dbe3d9c8bd51c10e353a63a8b06d7205
Andrea Vaiuso <andreavaiuso@community.unipa.it>
2022-01-25 17:04:56 +0100

    Commit test

---
+++ test.py
@@ -0,0 +1,19 @@
+
+def andrea():
+    pass
+
+def luca():
+    pass
+
+def salvo():
+    pass
+
+x = ""
+
+while 1:
+    if x == "andrea":
+        pass
+    elif x == "luca":
+        pass
+    elif x == "salvo":
+        pass
